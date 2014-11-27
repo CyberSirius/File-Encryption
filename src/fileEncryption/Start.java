0b5fbe6c0b24ed8dd149d468424418df2b70a6fb
CyberSirius <sirius.rashev@gmail.com>
2014-11-27 19:41:56 +0200

    Version 1.0

--- src/fileEncryption/Start.java
+++ src/fileEncryption/Start.java
@@ -8,16 +8,5 @@ public class Start {
 	public static void main(String[] args) {
 		FileEncoder61702 fileEncoder = new FileEncoder61702();
 		LinkedList<Character> key = new LinkedList<Character>();
-		for (int i = 255; i > -1; i--) {
-			key.add((char) i);
-		}
-		long time;
-		time = System.currentTimeMillis();
-		fileEncoder.encode("C:\\Users\\Strashimir\\Desktop\\test.txt",
-				"C:\\Users\\Strashimir\\Desktop\\testEnc.enc", key);
-		fileEncoder.decode("C:\\Users\\Strashimir\\Desktop\\testEnc.enc",
-				"C:\\Users\\Strashimir\\Desktop\\testDec.txt", key);
-		time = System.currentTimeMillis() - time;
-		System.out.println(time + " miliseconds");
 	}
 }
